6412742fd11743699e0d7da518b88b87670541c3
D1414 <Dominik.Lubos@gmail.com>
2025-05-13 14:08:45 +0200

    few fixes

--- script.js
+++ script.js
@@ -1,63 +1,75 @@
 const championGrid = document.getElementById("championGrid");
-// Riot Games API constants
-const API_KEY = "RGAPI-ee158dc7-30d9-41fb-a6e1-0499a265812d";
-const USERNAME = "odnamra";
-const GAME_TAG = "euw";
+const API_KEY = "RGAPI-32c0199c-efb0-4f04-8085-603de799c378";
 const API_BASE_URL = "https://europe.api.riotgames.com";
 const ICON_BASE_URL = "https://ddragon.leagueoflegends.com/cdn/15.9.1/img/champion/";
 
-// Function to fetch champion data
-async function fetchChampionData() {
-  const response = await fetch("https://ddragon.leagueoflegends.com/cdn/15.9.1/data/en_US/champion.json");
-  console.log(response);
-  return response.json();
+
+document.getElementById("form").addEventListener("submit", handleFormSubmit);
+
+async function handleFormSubmit(event) {
+  event.preventDefault();
+
+  const username = document.getElementById("username").value;
+  const gameTag = document.getElementById("gametag").value;
+
+  championGrid.innerHTML = "";
+  
+  try {
+    await getChampionsOfMasteries(username, gameTag);
+  } catch (error) {
+    console.error("Fehler beim Abrufen der Daten:", error);
+  }
 }
 
-// Function to fetch PUUID
+async function fetchChampionData() {
+  const champions = new Map();
+  const jsonFetch = await fetch(
+    "https://ddragon.leagueoflegends.com/cdn/15.9.1/data/en_US/champion.json"
+  );
+  const response = await jsonFetch.json();
+
+  for (const champion in response.data) {
+    champions.set(
+      response.data[champion].key,
+      [response.data[champion].image['full'], response.data[champion].name])
+  }
+
+  return champions;
+}
+
 async function fetchPUUID(username, gameTag) {
   const apiUrl = `${API_BASE_URL}/riot/account/v1/accounts/by-riot-id/${username}/${gameTag}/?api_key=${API_KEY}`;
   const response = await fetch(apiUrl);
   const data = await response.json();
-  console.log(data.puuid)
   return data.puuid;
 }
 
-// Function to fetch champion masteries
-async function fetchChampionMasteries(puuid) {
+async function fetchChampionMasteries(username, gameTag) {
+  var puuid = await fetchPUUID(username, gameTag);
+
   const apiUrl = `https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/${puuid}?api_key=${API_KEY}`;
   const response = await fetch(apiUrl);
+
   return response.json();
 }
+async function getChampionsOfMasteries(username, gameTag) {
+  const mastery = await fetchChampionMasteries(username, gameTag)
+  const champions = await fetchChampionData();
 
-// Function to generate champion icons
-async function generateChampionIcons() {
-  try {
-    const championsJson = await fetchChampionData();
-    const puuid = await fetchPUUID(USERNAME, GAME_TAG);
-    const masteries = await fetchChampionMasteries(puuid);
-    const champions = {};
-    for (const champion in championsJson.data) {
-      const championId = parseInt(championsJson.data[champion]["key"]);
-      champions[championId] = champion;
+  mastery.forEach(element => {
+    const champion = champions.get(element['championId'].toString());
+    const iconUrl = `${ICON_BASE_URL}${champion[0]}`
+    const championDiv = document.createElement("div");
+    championDiv.className = "champion";
+
+    let imageStyle = "";
+    if(element['championLevel'] < 5){
+      imageStyle = "filter: grayscale(100%); opacity: 0.8;";
     }
-    masteries.forEach((mastery) => {
-      const championId = mastery.championId;
-      const championName = champions[championId];
-      if (championName) {
-        const iconUrl = `${ICON_BASE_URL}${championName}.png`;
-        const championDiv = document.createElement("div");
-        championDiv.className = "champion";
-        championDiv.innerHTML = `
-          <img src="${iconUrl}" alt="${championName}" />
-          <p>${championName}</p>
+    championDiv.innerHTML = `
+          <img src="${iconUrl}" alt="${champion[1]}" style="${imageStyle}"/>
+          <p>${champion[1]}</p>
         `;
-        championGrid.appendChild(championDiv);
-      }
-    });
-  } catch (error) {
-    console.error("Error generating champion icons:", error);
-  }
+    championGrid.appendChild(championDiv);
+  });
 }
-
-// Initialize the script
-generateChampionIcons();
